08b140bae3306225bc80857ac6479662ec0faa85
Eliasib García <eliasib.lv.12@gmail.com>
2017-05-31 16:00:58 +0100

    Created controller, methods and routes for albums.

--- api-restful/app.js
+++ api-restful/app.js
@@ -6,12 +6,15 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // Carga de rutas
+var albumRoutes = require('./routes/album');
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Configuración de cabeceras
 
-// Configuración de rutas base y controladores
+// Configuración de rutas base
+app.use('/api', albumRoutes);
 
 module.exports = app;
