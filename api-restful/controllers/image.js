380bbe77e694c4e36fedb60401c2309175f464ac
Eliasib García <eliasib.lv.12@gmail.com>
2017-05-31 21:12:33 +0100

    Added methods and routes for getting all images and one image by id.
    - When getting one image, the album property is populated.

--- api-restful/controllers/image.js
+++ api-restful/controllers/image.js
@@ -3,10 +3,72 @@
 var Image = require('../models/image');
 var Album = require('../models/album');
 
-function pruebas(req, res) {
-    res.status(200).send({message: 'Pruebas de controlador de imagenes'});
+function getImages(req, res) {
+    Image.find((err, images) => {
+        if (err) {
+            res.status(500).send({message: 'Error en la peticion'});
+        }
+        else {
+            if (!images) {
+                res.status(404).send({message: 'No existen imagenes'});
+            }
+            else {           
+                res.status(200).send(images);
+            }
+        }
+    });
+}
+
+function getImage(req, res) {
+    var imageId = req.params.id;
+
+    Image.findById(imageId, (err, image) => {
+        if (err) {
+            res.status(500).send({message: 'Error en la peticion'});
+        }
+        else {
+            if (!image) {
+                res.status(404).send({message: 'No existe la imagen'});
+            }
+            else {
+                Album.populate(image, {path: 'album'}, (err, image) => {
+                    if (err) {
+                        res.status(500).send({message: 'Error en la peticion'});
+                    }
+                    else {
+                        res.status(200).send(image);
+                    }
+                });
+            }
+        }
+    });
+}
+
+function saveImage(req, res) {
+    var image = new Image();
+    
+    var params = req.body;
+    image.title = params.title;
+    image.picture = null;
+    image.album = params.album;
+
+    image.save((err, imageStored) => {
+        if (err) {
+            res.status(500).send({message: 'Error en la peticion'});
+        }
+        else {
+            if (!imageStored) {
+                res.status(404).send({message: 'No existe la imagen'});
+            }
+            else {
+                res.status(200).send(image);
+            }
+        }
+    });
 }
 
 module.exports = {
-    pruebas
+    getImages,
+    getImage,
+    saveImage
 }
--- api-restful/routes/image.js
+++ api-restful/routes/image.js
@@ -4,6 +4,8 @@ var express = require('express');
 var ImageController = require('../controllers/image');
 var api = express.Router();
 
-api.get('/prueba-image', ImageController.pruebas);
+api.get('/images', ImageController.getImages);
+api.get('/image/:id', ImageController.getImage);
+api.post('/image', ImageController.saveImage);
 
 module.exports = api;
