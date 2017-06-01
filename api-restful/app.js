37957e581925f9c98b4fffbca4cdb72fd3d38d25
Eliasib García <eliasib.lv.12@gmail.com>
2017-06-01 21:45:10 +0100

    Configured headers

--- api-restful/app.js
+++ api-restful/app.js
@@ -14,6 +14,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Configuración de cabeceras
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, content-Type, Accept, Access-Control-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE');
+    res.header('Allow', 'GET, POST, PUT, OPTIONS, DELETE');
+
+    next();
+});
 
 // Configuración de rutas base
 app.use('/api', albumRoutes);
