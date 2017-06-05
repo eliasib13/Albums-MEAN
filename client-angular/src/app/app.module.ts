5733043ae79b8e38cbe36d92e3fdfeee6c21fb3b
Eliasib García <eliasib.lv.12@gmail.com>
2017-06-05 14:58:26 +0100

    Configured router and added albums-list component

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { routing, appRoutingProviders } from './app.routing';
+
+import { AlbumsListComponent } from './components/albums-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumsListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
