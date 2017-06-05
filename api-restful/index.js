8f9a2725aab58d0a917c4ee8c037b765559f97eb
Eliasib García <eliasib.lv.12@gmail.com>
2017-06-05 13:17:53 +0100

    Using native promises due to deprecation warning on mpromise (mongoose's default promise library)

--- api-restful/index.js
+++ api-restful/index.js
@@ -4,6 +4,9 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3700;
 
+// Using native promises due to deprecation warning on mpromise (mongoose's default promise library)
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost:27017/app_albums', (err, res) => {
     if (err) {
         throw err;
