446c0482cac9ff5e3e41ea4f168d3d0ed137fe82
Eliasib García <eliasib.lv.12@gmail.com>
2017-06-15 10:58:07 +0100

    Creating models and AlbumService.

---
+++ client-angular/src/app/services/album.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Album } from '../models/album';
+import { GLOBAL } from './global';
+
+@Injectable()
+export class AlbumService {
+    public url: string;
+
+    constructor (private _http: Http) {
+        this.url = GLOBAL.url;
+    }
+
+    getAlbums() {
+        return this._http.get(this.url + 'albums')
+                        .map(res => res.json());
+    }
+}
---
+++ client-angular/src/app/services/global.ts
@@ -0,0 +1,4 @@
+export var GLOBAL = {
+    url: 'http://loclahost:3700/api/',
+    ip: '127.0.0.1'
+}
