96b933078afe6f93c194fdaf6f694517f2f33b32
Eliasib García <eliasib.lv.12@gmail.com>
2017-10-24 15:04:53 +0100

    Added methods on ImageService.

--- client-angular/src/app/services/image.service.ts
+++ client-angular/src/app/services/image.service.ts
@@ -13,6 +13,21 @@ export class ImageService {
         this.url = GLOBAL.url;
     }
 
+    getImages(albumId = null) {
+        if (albumId == null) {
+            return this._http.get(this.url + 'images')
+                             .map(res => res.json());
+        } else {
+            return this._http.get(this.url + 'images/' + albumId)
+                             .map(res => res.json());
+        }
+    }
+
+    getImage(id) {
+        return this._http.get(this.url + 'image/' + id)
+                         .map(res => res.json());
+    }
+
     addImage(image: Image) {
         let json = JSON.stringify(image);
         let params = json;
@@ -20,5 +35,19 @@ export class ImageService {
 
         return this._http.post(this.url + 'image', params, {headers: headers})
                             .map(res => res.json());
+    }
+
+    editImage(id: string, image: Image) {
+        let json = JSON.stringify(image);
+        let params = json;
+        let headers = new Headers({"Content-Type": "application/json"});
+
+        return this._http.put(this.url + 'image/' + id, params, {headers: headers})
+                            .map(res => res.json());
+    }
+
+    deleteImage(id: string) {
+        return this._http.delete(this.url + 'image/' + id)
+                         .map(res => res.json());
     }
 }
